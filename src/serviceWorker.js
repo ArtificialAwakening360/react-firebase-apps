bd993b235135a102ed9079a52349e1a86dd70c0d
Dazai Osamu <stacruz1820002@mkt.ceu.edu.ph>
2021-10-18 11:48:53 -0800

    All errors fixed

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -24,8 +24,9 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
+      const reg = registration;
+      reg.onupdatefound = () => {
+        const installingWorker = reg.installing;
         if (installingWorker == null) {
           return;
         }
@@ -35,33 +36,33 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
+              /* console.log(
                 'New content is available and will be used when all '
                   + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
-              );
+              ); */
 
               // Execute callback
               if (config && config.onUpdate) {
-                config.onUpdate(registration);
+                config.onUpdate(reg);
               }
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              // console.log('Content is cached for offline use.');
 
               // Execute callback
-              if (config && config.onSuccess) {
-                config.onSuccess(registration);
-              }
+              // if (config && config.onSuccess) {
+              //  config.onSuccess(reg);
+              // }
             }
           }
         };
       };
-    })
-    .catch((error) => {
-      console.log('Error during service worker registration:', error);
     });
+  // .catch((error) => {
+  //  console.error('Error during service worker registration:', error);
+  // });
 }
 
 function checkValidServiceWorker(swUrl, config) {
@@ -88,9 +89,9 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.',
-      );
+      // console.log(
+      //  'No internet connection found. App is running in offline mode.',
+      // );
     });
 }
 
@@ -115,10 +116,10 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service '
-              + 'worker. To learn more, visit https://bit.ly/CRA-PWA',
-          );
+          // console.log(
+          //  'This web app is being served cache-first by a service '
+          //    + 'worker. To learn more, visit https://bit.ly/CRA-PWA',
+          // );
         });
       } else {
         // Is not localhost. Just register service worker
@@ -133,9 +134,9 @@ export function unregister() {
     navigator.serviceWorker.ready
       .then((registration) => {
         registration.unregister();
-      })
-      .catch((error) => {
-        console.error(error.message);
       });
+    // .catch((error) => {
+    //  console.error(error.message);
+    // });
   }
 }
