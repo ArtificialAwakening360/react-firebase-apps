7d8162c89914adc9bbc998b70b46d77ef2c2d573
Dazai Osamu <stacruz1820002@mkt.ceu.edu.ph>
2021-10-18 19:25:07 -0800

    Updates

---
+++ precache-manifest.733a4c17a434b1410754c71bd69eaf3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3472dbefa5c5154fb5ac6b102f619cdd",
+    "url": "/react-firebase-apps/index.html"
+  },
+  {
+    "revision": "800cdb39e661d52422ce",
+    "url": "/react-firebase-apps/static/css/main.d5c0ca3d.chunk.css"
+  },
+  {
+    "revision": "f91198679fb96d21a4aa",
+    "url": "/react-firebase-apps/static/js/2.0a1e3d91.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/react-firebase-apps/static/js/2.0a1e3d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800cdb39e661d52422ce",
+    "url": "/react-firebase-apps/static/js/main.e6510ced.chunk.js"
+  },
+  {
+    "revision": "65aa54529377b15b6ab5",
+    "url": "/react-firebase-apps/static/js/runtime-main.05b4c559.js"
+  }
+]);
